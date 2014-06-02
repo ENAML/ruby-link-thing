27820513abb8e0bd25f673279d4904bb282da501
ENAML <EthanACline@gmail.com>
2014-06-02 15:05:59 -0400

    added some gems to gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :visits
+
+  resources :link_items
+
+  get ':short_name', to: "link_items#redirector"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
